c2e0f2f3254f20d30cccadda12807178865a8620
HANNIEL21 <dhanniel21@gmail.com>
2022-05-08 12:33:17 +0100

    floating nav bar

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,21 @@ import "./about.scss";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
